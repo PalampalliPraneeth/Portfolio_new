69488d8f663fd84397d6deec5d7d0e87f5e6fbfa
praneeth reddy <praneeth.reddy@tudip.com>
2024-03-10 16:22:37 -0400

    changes to skills

--- src/components/ContactMe/ContactInfoCard/ContactInfoCard.jsx
+++ src/components/ContactMe/ContactInfoCard/ContactInfoCard.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import "./ContactInfoCard.css";
 
 const ContactInfoCard = ({ iconUrl, text }) => {
+  const handleEmailClick = () => {
+    window.location.href = `mailto:${text}`;
+  };
+
   return (
-    <div className="contact-details-card">
+    <div className="contact-details-card" onClick={handleEmailClick}>
       <div className="icon">
         <img src={iconUrl} alt={text} />
       </div>
