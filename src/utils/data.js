0e7fcd16b861494d605576359681d3360dfb7a7d
praneeth reddy <praneeth.reddy@tudip.com>
2024-03-11 21:12:59 -0400

    removed commented files

--- src/utils/data.js
+++ src/utils/data.js
@@ -106,34 +106,4 @@ export const WORK_EXPERIENCE = [
       gitUrl: "https://github.com/PalampalliPraneeth/Weather-application",
       webUrl: "https://weather-application-beta-self.vercel.app/"
     },
-  // {
-  //   title: "Senior Frontend Developer at Tech Innovators",
-  //   date: "January 2022 - Present",
-  //   responsibilities: [
-  //     "Led a team of frontend developers in the redesign of the company's flagship product, resulting in a 30% increase in user satisfaction.",
-  //     "Implemented advanced CSS animations and transitions to enhance the overall user experience.",
-  //     "Collaborated with UX/UI designers to translate wireframes and mockups into responsive and interactive web applications.",
-  //     "Introduced and integrated Vue.js into the tech stack, improving code maintainability and development efficiency.",
-  //   ],
-  // },
-  // {
-  //   title: "Junior Web Developer at Digital Solution",
-  //   date: "June 2020 - December 2021",
-  //   responsibilities: [
-  //     "Developed and maintained client websites, ensuring high-quality standards and timely project delivery.",
-  //     "Implemented SEO best practices, leading to a 25% increase in organic search traffic for key clients.",
-  //     "Worked closely with the design team to create pixel-perfect, cross-browser-compatible web pages.",
-  //     "Conducted code reviews and mentored junior developers in adopting best coding practices.",
-  //   ],
-  // },
-  // {
-  //   title: "Frontend Intern at Web Studio",
-  //   date: "May 2019 - August 2019",
-  //   responsibilities: [
-  //     "Assisted in the development of a new e-commerce platform, contributing to the implementation of frontend features using React.js.",
-  //     "Conducted performance optimization tasks, resulting in a 15% improvement in page load times.",
-  //     "Participated in daily stand-up meetings and collaborated with senior developers to troubleshoot and resolve coding issues.",
-  //     "Gained hands-on experience with version control systems, specifically Git, in a collaborative development environment.",
-  //   ],
-  // },
 ];
